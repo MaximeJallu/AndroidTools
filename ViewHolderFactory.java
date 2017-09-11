6e1aeec72972da72c407fce4d5639cee80d5b059
Jallu <jallumaxime@gmail.com>
2017-09-11 14:20:17 +0200

    Update ViewHolderFactory

--- ViewHolderFactory.java
+++ ViewHolderFactory.java
@@ -15,30 +15,80 @@ import java.lang.ref.WeakReference;
  * Use this Class for : <br/>
  * ... {DOCUMENTATION}
  */
-public abstract class ViewHolderFactory<T> {
+public class ViewHolderFactory<T> {
 
-    protected WeakReference<IBaseCommunication> mCommunication;
+    private static final String TAG = ViewHolderFactory.class.getName();
+    private WeakReference<IBaseCommunication> mCommunication;
+    private Class<? extends RecyclerViewHolder<T>> mViewHolderType;
+    private HashMap<Integer, Class<? extends RecyclerViewHolder<T>>> mClassHashMap;
 
-    void setCommunication(IBaseCommunication communication) {
-        mCommunication = new WeakReference<>(communication);
+    public ViewHolderFactory() {
+        this(null);
+    }
+
+    @SuppressLint("UseSparseArrays")
+    public ViewHolderFactory(Class<? extends RecyclerViewHolder<T>> viewHolderType) {
+        mViewHolderType = viewHolderType;
+        mClassHashMap = new HashMap<>();
+    }
+
+    final RecyclerViewHolder<T> createVH(View view, int viewType) {
+        RecyclerViewHolder<T> ret = getInstance(view, viewType);
+        if (ret != null) {
+            ret.setCommunication(getInterfaceCallback());
+        }
+        return ret;
+    }
+
+    @SuppressWarnings("TryWithIdenticalCatches")
+    private RecyclerViewHolder<T> getInstance(View view, int viewType) {
+        RecyclerViewHolder<T> ret = null;
+        try {
+            ret = getViewHolderType(viewType).getConstructor(View.class).newInstance(view);
+        } catch (InstantiationException e) {
+            Log.i(TAG, "getInstance: ");
+        } catch (IllegalAccessException e) {
+            Log.i(TAG, "getInstance: ", e);
+        } catch (InvocationTargetException e) {
+            Log.i(TAG, "getInstance: ", e);
+        } catch (NoSuchMethodException e) {
+            Log.i(TAG, "not found constructor. La class suivante doit être en static ou ne pas être en inner class : " + getViewHolderType(viewType).getName(), e);
+        }
+        return ret;
+    }
+
+    protected <I extends IBaseCommunication> I getInterfaceCallback() {
+        I i = null;
+        try {
+            //noinspection unchecked
+            i = (I) getCommunication();
+        } catch (ClassCastException e) {
+            Log.e(TAG, "getInterfaceCallback: ", e);
+        }
+        return i;
+    }
+
+    protected Class<? extends RecyclerViewHolder<T>> getViewHolderType(int viewType) {
+        Class<? extends RecyclerViewHolder<T>> vm = mViewHolderType;
+        if (mClassHashMap.containsKey(viewType)) {
+            vm = mClassHashMap.get(viewType);
+        }
+        return vm;
     }
 
     public final IBaseCommunication getCommunication() {
         return mCommunication.get();
     }
 
-    protected <I extends IBaseCommunication> I getInterfaceCallback(){
-        I i = null;
-        try {
-            //noinspection unchecked
-            i = (I) getCommunication();
-        }catch (ClassCastException e){
-            Log.e("ViewHolderFactory", "getInterfaceCallback: ", e);
-        }
-        return i;
+    protected void setCommunication(IBaseCommunication communication) {
+        mCommunication = new WeakReference<>(communication);
     }
 
-    abstract protected RecyclerViewHolder<T> createVH(View view, int viewType);
+    final @LayoutRes int getLayoutRes(int viewType) {
+        return getViewHolderType(viewType).getAnnotation(BindLayoutRes.class).value();
+    }
 
-    abstract protected @LayoutRes int getLayoutRes(int viewType);
+    public void putViewType(int viewType, Class<? extends RecyclerViewHolder<T>> viewHolder){
+        mClassHashMap.put(viewType, viewHolder);
+    }
 }
